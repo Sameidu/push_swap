b2b57c1061f8419f28d0fc1ae4bed68484b6ba02
Sara Meixoeiro durante <smeixoei@c2r7s5.42madrid.com>
2023-10-20 18:33:42 +0200

    pushh

--- push.c
+++ push.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: smeixoei <smeixoei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:24:12 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/10/20 09:50:37 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:33:25 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,51 @@
 // Puedo pasar por argumento cual de los dos stack quiero haccer que se mueva y así evitar hcer la misma función para cada uno.
 // pa push a: Toma el primer elemento del stack b y lo pone el primero en el stack a. No hace nada si b está vacío.
 // pb push b: Toma el primer elemento del stack a y lo pone el primero en el stack b. No hace nada si a está vacío.
-void	ft_push(t_stack **stack_a, t_stack **stack_b, char c)
+void	ft_pusha(t_stack **stack_a, t_stack **stack_b)
 {
-    t_stack	*aux;
-	t_stack *aux2;
+    t_stack	*node;
 
-	if (stack_a == NULL)
+	if (*stack_b == NULL)
 		return ;
-	aux = stack_a;
+	node = *stack_b;
+	*stack_b = (*stack_b)->next;
+	if ((*stack_a)->next == NULL)
+	{
+		*stack_a = node;
+		node = node->next;
+		node = node->past;
+	}
+	else
+	{
+		node->next = (*stack_a);
+		node->past = (*stack_a)->past;
+		(*stack_a)->past->next = node;
+		*stack_a = (*stack_a)->next;
+		*stack_a = node;
+	}
+}
+
+void	ft_pushb(t_stack **stack_a, t_stack **stack_b)
+{
+    t_stack	*node;
+
+	if (*stack_a == NULL)
+		return ;
+	node = *stack_a;
+	*stack_a = (*stack_a)->next;
+	node->next = NULL;
+	if ((*stack_a)->next == NULL)
+	{
+		*stack_b = node;
+		node = node->next;
+		node = node->past;
+	}
+	else
+	{
+		node->next = (*stack_b);
+		node->past = (*stack_b)->past;
+		(*stack_b)->past->next = node;
+		*stack_b = (*stack_b)->next;
+		*stack_b = node;
+	}
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: smeixoei <smeixoei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:20:13 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/10/17 16:20:14 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:33:26 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include "./libft/libft.h"
 
 typedef struct s_stack
 {
@@ -26,5 +27,7 @@ typedef struct s_stack
 
 void	ft_error(char *str);
 void	ft_isnum(char *str);
+void	ft_swap(t_stack **stack, char c);
+void	ft_swap_ab(t_stack **a, t_stack **b);
 
 #endif
