10c5a252079cdd3b0094020521de10025a2ee6ee
Sara Meixoeiro durante <smeixoei@c2r6s6.42madrid.com>
2023-10-24 15:30:05 +0200

    norm

--- push.c
+++ push.c
@@ -3,41 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: smeixoei <smeixoei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:24:12 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/10/24 11:45:08 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:26:13 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ft_push(t_stack **push, t_stack **dst, char c)
+void	ft_push(t_stack **push, t_stack **dst, char c)
 {
-    t_stack *node;
+	t_stack	*node;
 
 	if (*push == NULL)
-        return;
-	node = *push;
-    *push = (*push)->next;
-    node->next = NULL;
-    if (*dst == NULL)
-    {
-        *dst = node;
-        node->next = node->past = node;
-    }
-    else
-    {
-        node->next = (*dst);
-        node->past = (*dst)->past;
-        (*dst)->past->next = node;
-        (*dst)->past = node;
-        *dst = node;
-    }
-    if (c == 'a')
-        ft_putendl_fd("pa", 2);
-    else if (c == 'b')
-        ft_putendl_fd("pb", 2);
-    else
-        ft_putendl_fd("ERROR: BAD STACK", 2);
+		return ;
+	*push = ((node = *push), (*push)->next);
+	node->next = NULL;
+	if (*dst == NULL)
+	{
+		*dst = node;
+		node->next = node->past = node;
+	}
+	else
+	{
+		node->next = (*dst);
+		node->past = (*dst)->past;
+		(*dst)->past->next = node;
+		(*dst)->past = node;
+		*dst = node;
+	}
+	if (c == 'a')
+		ft_putendl_fd("pa", 2);
+	else if (c == 'b')
+		ft_putendl_fd("pb", 2);
+	else
+		ft_putendl_fd("ERROR: BAD STACK", 2);
 }
--- swap.c
+++ swap.c
@@ -3,23 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: smeixoei <smeixoei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:24:49 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/10/24 10:35:43 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:28:17 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// sa swap a: Intercambia los dos primeros elementos del stack a. No hace nada si hay uno o menos elementos.
-// sb swap b: Intercambia los dos primeros elementos del stack b. No hace nada si hay uno o menos elementos.
 void	ft_swap(t_stack **stack, char c)
 {
 	t_stack	*aux;
 	t_stack	*aux_2;
 
-    if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 		return ;
 	aux = (*stack);
 	aux_2 = (*stack)->next;
@@ -30,15 +28,14 @@ void	ft_swap(t_stack **stack, char c)
 	else if (c == 'b')
 		ft_putendl_fd("sb", 2);
 	else if (c == 'x')
-			return ;
+		return ;
 	else
 		ft_putendl_fd("ERROR: BAD STACK", 2);
 }
 
-// ss swap a y swap b a la vez.
 void	ft_swap_ab(t_stack **a, t_stack **b, char c)
 {
-    ft_swap(a, 'x');
+	ft_swap(a, 'x');
 	ft_swap(b, 'x');
 	ft_putendl_fd("ss", 2);
 }
