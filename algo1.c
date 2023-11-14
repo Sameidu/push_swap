ac85b85e6ad440833de7bf433f6c359cf6de5b80
Sara Meixoeiro <sarameixoeiro@Puesto-10.local>
2023-11-14 11:55:43 +0100

    3

---
+++ algo1.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo1.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 12:18:25 by smeixoei          #+#    #+#             */
+/*   Updated: 2023/11/14 11:22:23 by smeixoei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "push_swap.h"
+
+void	ft_sort3(t_stack **stack)
+{
+	t_stack	*current;
+	t_stack	*next;
+	t_stack	*prev;
+
+	current = *stack;
+	next = current->next;
+	prev = current->past;
+	if (current->content > next->content && current->content < prev->content)
+		ft_swap(stack, 'a');
+	else if (current->content > next->content && current->content > prev->content)
+	{
+		ft_swap(stack, 'a');
+		ft_reverse_rotate(stack, 'a');
+	}
+	else if (current->content < next->content && current->content > prev->content)
+		ft_rotate(stack, 'a');
+	else if (current->content < next->content && current->content < prev->content)
+	{
+		ft_swap(stack, 'a');
+		ft_rotate(stack, 'a');
+	}
+}
